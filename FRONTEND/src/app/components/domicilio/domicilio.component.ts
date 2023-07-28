5d81fff7c3b482b73a4f896d561d75acb64c583c
Daniel Díaz Ceballos <99226131+dany6x@users.noreply.github.com>
2023-07-28 00:20:59 -0600

    Update domicilio.component.ts

--- FRONTEND/src/app/components/domicilio/domicilio.component.ts
+++ FRONTEND/src/app/components/domicilio/domicilio.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'; 
 //Importar para formularios
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 //Importar el servicio de DomicilioService
